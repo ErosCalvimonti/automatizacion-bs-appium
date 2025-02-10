75d96a0d0a0b0298c1da57dfd7a6e9fcedf9943f
ErosCalvimonti <karielcode@gmail.com>
2025-02-10 15:25:50 -0300

    add initial test specifications for Android elements

---
+++ test/specs/android/androidFindElements.spec.js
@@ -0,0 +1,10 @@
+describe('Pruebas apk', () => {
+    it(' Prueba basica', async ()=> {
+        //crear una espera implicita
+        await driver.pause(5000)
+    } )
+
+    it('Ver ultimos movimientos', async ()=> {
+        await driver.pause(5000)
+    })
+})
